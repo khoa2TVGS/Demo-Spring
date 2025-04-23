00300b135243b5a99b3fd852b89f9c737ae7678a
Kerr <ng.nguyenkhoa2006@gmail.com>
2025-04-23 10:04:22 +0700

    forget some code xD

--- src/main/java/com/webserver/demo_spring/WebController.java
+++ src/main/java/com/webserver/demo_spring/WebController.java
@@ -1,4 +1,20 @@
 package com.webserver.demo_spring;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String hello() {
+        return "login";
+    }
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 }
